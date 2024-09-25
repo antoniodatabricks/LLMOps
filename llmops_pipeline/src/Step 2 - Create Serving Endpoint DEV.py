796c35022a9e7321e6a791a9e6bce8fe35a8779c
antonio.pinheirofilho@databricks.com <antonio.pinheirofilho@databricks.com>
2024-09-25 21:18:24 +0000

    Fix parameter

--- llmops_pipeline/src/Step 2 - Create Serving Endpoint DEV.py
+++ llmops_pipeline/src/Step 2 - Create Serving Endpoint DEV.py
@@ -1,5 +1,6 @@
 # Databricks notebook source
-host = dbutils.widgets.get("host")
+endpoint_name = dbutils.widgets.get("endpoint_name")
+endpoint_api = dbutils.widgets.get("endpoint_api")
 endpoint_token_scope = dbutils.widgets.get("endpoint_token_scope")
 endpoint_token_secret = dbutils.widgets.get("endpoint_token_secret")
 endpoint_token = dbutils.secrets.get(scope=endpoint_token_scope, key=endpoint_token_secret)
@@ -7,7 +8,6 @@ host_dependency = dbutils.widgets.get("host_dependency")
 endpoint_token_dependency_scope = dbutils.widgets.get("endpoint_token_dependency_scope")
 endpoint_token_dependency_secret = dbutils.widgets.get("endpoint_token_dependency_secret")
 endpoint_token_dependency = dbutils.secrets.get(scope=endpoint_token_dependency_scope, key=endpoint_token_dependency_secret)
-endpoint_name = dbutils.widgets.get("endpoint_name")
 tracking_table_catalog = dbutils.widgets.get("tracking_table_catalog")
 tracking_table_schema = dbutils.widgets.get("tracking_table_schema")
 tracking_table_name = dbutils.widgets.get("tracking_table_name")
@@ -41,7 +41,7 @@ data = {
 headers = {"Context-Type": "text/json", "Authorization": f"Bearer {endpoint_token}"}
 
 response = requests.post(
-    url=f"{host}/serving-endpoints/{endpoint_name}/invocations", json=data, headers=headers
+    url=endpoint_api, json=data, headers=headers
     )
 
 # Assert that the status code indicates success (2xx range)
